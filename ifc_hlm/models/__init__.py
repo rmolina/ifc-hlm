5ba66ed5c8ff2112a51619e1e7b841f47d099aaf
Ruben Molina <rmolina@gmail.com>
2025-10-15 07:31:40 -0500

    Add Model1 (Model252 reporting fluxes. Routing disabled).

--- ifc_hlm/models/__init__.py
+++ ifc_hlm/models/__init__.py
@@ -1,9 +1,11 @@
 """Models module."""
 
+from .model1 import Model1
 from .model252 import Model252
 from .model254 import Model254
 
 __all__ = [
+    "Model1",
     "Model252",
     "Model254",
 ]
